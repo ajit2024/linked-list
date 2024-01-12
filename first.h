587e10da938ceae650675003c4dd0dab05bd24a0
ajit sharma <rockyajit94@gmail.com>
2024-01-12 13:46:14 +0530

    testing to push into github

---
+++ first.h
@@ -0,0 +1,19 @@
+#ifndef FIRST_H_INCLUDED
+#define FIRST_H_INCLUDED
+
+#include<iostream>
+using namespace std;
+
+int x=6;
+
+void func1(void)
+{
+    cout<<"in func1(), the value of x: "<<x<<endl;
+}
+
+void func2(void)
+{
+    cout<<"in func2(), the value of x: "<<x<<endl;
+}
+
+#endif // FIRST_H_INCLUDED
